38d70190b0ba5ab5a777a9fa7425ea72f3f8e598
chenxuuu <lolicon@papapoi.com>
2018-10-20 19:10:57 +0800

    change to cli

---
+++ mailNetDisk-cli/mailNetDisk-cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace mailNetDisk_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
