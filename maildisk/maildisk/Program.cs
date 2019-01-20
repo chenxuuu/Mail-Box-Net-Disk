e4ce2d40fedae14ce374113f672ff8bcd62cd8ca
chenxuuu <lolicon@papapoi.com>
2019-01-20 17:31:46 +0800

    增加mail对象，添加new与打开文件夹方法

--- maildisk/maildisk/Program.cs
+++ maildisk/maildisk/Program.cs
@@ -2,6 +2,7 @@
 using MailKit.Net.Imap;
 using MailKit.Search;
 using System;
+using maildisk.apis;
 
 namespace maildisk
 {
@@ -12,39 +13,44 @@ namespace maildisk
             foreach (var i in args)
                 Console.WriteLine(i);
 
+            var mail = new MailClient("imap.qq.com", 993, true, 
+                "lolicon@papapoi.com", "lsykvlybakgkbfda", 
+                "lolicon@papapoi.com");
+            //get folder
+            var folder = mail.GetFolder("其他文件夹/se");
+
+
+
             using (var client = new ImapClient())
             {
-                // For demo-purposes, accept all SSL certificates
-                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                //// For demo-purposes, accept all SSL certificates
+                //client.ServerCertificateValidationCallback = (s, c, h, e) => true;
 
-                client.Connect("imap.qq.com", 993, true);
+                //client.Connect("imap.qq.com", 993, true);
 
-                client.Authenticate("lolicon@papapoi.com", "lsykvlybakgkbfda");
-
-
-                var personal = client.GetFolder(client.PersonalNamespaces[0]);
-                foreach (var folder in personal.GetSubfolders(false))
-                {
-                    Console.WriteLine("[folder] {0}", folder.Name);
-                    foreach (var f in folder.GetSubfolders(false))
-                        Console.WriteLine("[folder] {0}", folder.Name + "/" + f.Name);
-                }
+                //client.Authenticate("lolicon@papapoi.com", "lsykvlybakgkbfda");
+                
+                //var personal = client.GetFolder(client.PersonalNamespaces[0]);
+                //foreach (var folder in personal.GetSubfolders(true))
+                //{
+                //    Console.WriteLine("[folder] {0}", folder.Name);
+                //}
 
                 
-                // The Inbox folder is always available on all IMAP servers...
-                var inbox = client.GetFolder("其他文件夹/facebook");
-                inbox.Open(FolderAccess.ReadOnly);
+                //// The Inbox folder is always available on all IMAP servers...
+                //var inbox = client.GetFolder("其他文件夹/facebook");
+                //inbox.Open(FolderAccess.ReadOnly);
 
-                Console.WriteLine("Total messages: {0}", inbox.Count);
-                Console.WriteLine("Recent messages: {0}", inbox.Recent);
+                //Console.WriteLine("Total messages: {0}", inbox.Count);
+                //Console.WriteLine("Recent messages: {0}", inbox.Recent);
                 
-                for(int i = 0;i<inbox.Count;i+=100)
-                {
-                    foreach (var summary in inbox.Fetch(i, i + 100, MessageSummaryItems.Envelope | MessageSummaryItems.UniqueId))
-                    {
-                        Console.WriteLine("[summary] {0:D2}: {1}", summary.Index, summary.Envelope.Subject);
-                    }
-                }
+                //for(int i = 0;i<inbox.Count;i+=100)
+                //{
+                //    foreach (var summary in inbox.Fetch(i, i + 100, MessageSummaryItems.Full | MessageSummaryItems.UniqueId))
+                //    {
+                //        Console.WriteLine("[summary] {0:D2}: {1}", summary.Index, summary.Envelope.Subject);
+                //    }
+                //}
                 
 
 
---
+++ maildisk/maildisk/apis/MailClient.cs
@@ -0,0 +1,77 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace maildisk.apis
+{
+    class MailClient
+    {
+        private string address;
+        private ImapClient client = new ImapClient();
+
+        /// <summary>
+        /// new mail client
+        /// </summary>
+        /// <param name="server">server address</param>
+        /// <param name="port">server port</param>
+        /// <param name="useSsl">server use ssl or not</param>
+        /// <param name="account">your account</param>
+        /// <param name="password">your password</param>
+        /// <param name="address">your email address</param>
+        public MailClient(string server, int port, bool useSsl, string account, string password, string address)
+        {
+            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+            client.Connect(server, port, useSsl);
+            client.Authenticate(account, password);
+            this.address = address;
+#if DEBUG
+            Console.WriteLine($"[mail create]mail client created, on {server}:{port}" +
+                $", ssl:{useSsl}, {account}, {address}");
+#endif
+        }
+
+        /// <summary>
+        /// get all folders in this mail
+        /// </summary>
+        /// <param name="path">path, default is empty</param>
+        /// <returns>folder list</returns>
+        public IMailFolder[] GetFolders(string path = "")
+        {
+            ArrayList folders = new ArrayList();
+            
+            var personal = client.GetFolder(path);
+
+            foreach (var folder in personal.GetSubfolders(false))
+            {
+                if (folder.GetSubfolders(false).Count > 0)
+                {
+#if DEBUG
+                    Console.WriteLine($"[folder open] {path + folder.Name}");
+#endif
+                    folders.AddRange(GetFolders(folder.FullName));
+                }
+                else
+                {
+                    folders.Add(folder);
+#if DEBUG
+                    Console.WriteLine($"[folder get] {folder.FullName}");
+#endif
+                }
+            }
+            return (IMailFolder[])folders.ToArray(typeof(IMailFolder));
+        }
+
+        /// <summary>
+        /// get one folder
+        /// </summary>
+        /// <param name="path">path, default is empty</param>
+        /// <returns>folder</returns>
+        public IMailFolder GetFolder(string path)
+        {
+            return client.GetFolder(path);
+        }
+    }
+}
